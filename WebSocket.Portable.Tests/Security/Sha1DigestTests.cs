02dfb2d9b763832d01b03164e4d9b6b1308488fc
esskar <sk@intertivity.com>
2014-06-04 16:20:56 +0200

    portable sha1 implementation

---
+++ WebSocket.Portable.Tests/Security/Sha1DigestTests.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Text;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using WebSocket.Portable.Security;
+
+namespace WebSocket.Portable.Tests.Security
+{
+    [TestClass]
+    public class Sha1DigestTests
+    {
+        [TestMethod]
+        public void TestVectors()
+        {
+            AssertSha1("The quick brown fox jumps over the lazy dog", "L9ThxnotKPzthJ7hu3bnORuT6xI=");
+            AssertSha1("The quick brown fox jumps over the lazy cog", "3p8sf9JeGzr60+haC9F9mxANtLM=");
+            AssertSha1("", "2jmj7l5rSw0yVb/vlWAYkK/YBwk=");
+        }
+
+        private static void AssertSha1(string input, string expected)
+        {
+            var hash = Sha1Digest.ComputeHash(Encoding.ASCII.GetBytes(input));
+            var actual = Convert.ToBase64String(hash);
+
+            Assert.AreEqual(expected, actual, "Invalid hash for input '{0}'", input);
+        }
+    }    
+}
