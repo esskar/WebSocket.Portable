e56467ab5f60cd0b69c088180277fb17ab553e80
esskar <sk@intertivity.com>
2014-06-04 16:31:42 +0200

    add core project

---
+++ WebSocket.Portable.Core/Properties/AssemblyInfo.cs
@@ -0,0 +1,13 @@
+﻿using System.Resources;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+
+[assembly: AssemblyTitle("WebSocket.Portable.Core")]
+[assembly: AssemblyProduct("WebSocket.Portable.Core")]
+[assembly: AssemblyCopyright("Copyright © 2014")]
+[assembly: NeutralResourcesLanguage("en")]
+
+[assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
+
+[assembly: InternalsVisibleTo("WebSocket.Portable.Tests")]
