30588f5f948550909f0d6496f60c0197418b9499
NVentimiglia <nick@simplesys.us>
2015-07-05 16:01:06 -0700

    Rebuilt after my pull was corrupted. Added non functional Windows Universal Example.

---
+++ Examples/WebSocket.Universal/WebSocket.Universal/WebSocket.Universal.Windows/MainPage.xaml.cs
@@ -0,0 +1,82 @@
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using System.Threading.Tasks;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+using WebSocket.Portable;
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=234238
+
+namespace WebSocket.Universal
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+
+            // socket Test
+            TestRda();
+
+            // Websocket Test
+            TestWebsocketPortable();
+        }
+
+
+        private async Task TestRda()
+        {
+            var socket = new Sockets.Plugin.TcpSocketClient();
+
+            //Never Ending
+            await socket.ConnectAsync("ws://echo.websocket.org", 80, false);
+
+            Debug.WriteLine("TestRda");
+        }
+
+        private async Task TestWebsocketPortable()
+        {
+            var client = new WebSocketClient();
+            client.Opened += websocket_Opened;
+            client.Closed += websocket_Closed;
+            client.MessageReceived += websocket_MessageReceived;
+
+            //Never Ending
+            await client.OpenAsync("ws://echo.websocket.org");
+
+            await client.SendAsync("Hello World");
+
+            await client.SendAsync("Hello World2");
+
+            Debug.WriteLine("TestWebsocketPortable");
+        }
+
+        void websocket_MessageReceived(Portable.Interfaces.IWebSocketMessage obj)
+        {
+            Debug.WriteLine(obj.ToString());
+        }
+
+        void websocket_Closed()
+        {
+            Debug.WriteLine("websocket_Opened");
+        }
+
+        void websocket_Opened()
+        {
+            Debug.WriteLine("websocket_Opened");
+        }
+    }
+}
